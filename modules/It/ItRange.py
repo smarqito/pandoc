88ad141c53d3503e67ac385429cd42974738c1e4
Marco António Sousa <smarqito@gmail.com>
2022-05-14 12:12:39 +0100

    It Module

---
+++ modules/It/ItRange.py
@@ -0,0 +1,12 @@
+from modules.It.ItVar import ItVar
+
+
+class ItRange(ItVar):
+    def __init__(self, default, _range, kws = [], end = None) -> None:
+        self.n, self.m = _range
+        super().__init__(kws, default, end)
+    
+    def handleDefault(self):
+        super().handleDefault()
+        if self.default:
+            self.default = "".join(list(self.default)[self.n : self.m + 1 if self.m else None])
