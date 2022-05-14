ce8ce8b40da083d58a66db9d57bf91c9e2a1da59
Marco António Sousa <smarqito@gmail.com>
2022-05-14 01:06:56 +0100

    pipe
    Co-authored-by: Miguel Ângelo Ferreira Fernandes <MiguelFernandes13@users.noreply.github.com>
    Co-authored-by: José Malheiro <JosefAM@users.noreply.github.com>

--- modules/Pipe.py
+++ modules/Pipe.py
@@ -1,33 +1,118 @@
+from re import sub
+from math import ceil, floor
+
 def pairs(x):
-    return x
-def rest(x):
-    return x
+    res = []
+    if type(x) is list:
+        ind = 1
+        for elem in x:
+            res.append({ ind : elem})
+            ind += 1 
+    elif type(x) is dict:
+         for key in x:
+            value = x[key]
+            if type(value) is dict:
+                value = pairs(x[key])
+            res.append({key : value})
+    return res
+    
 def reverse(x):
     return x[::-1]
 def first(x):
     return x[0]
 def last(x):
-    return x[len(x) - 1]
+    return x[-1]
 def rest(x):
-    return x[-(len(x) - 1):]
+    return x[1:]
 def allbutlast(x):
-    return x
+    return x[:-1]
 def chomp(x):
-    return x
+    return sub(r"\n+", "\n", x)
 def nowrap(x):
     return x
 def alpha(x):
-    return x
+    return sub(r"(2[0-6]|1\d|[1-9])", lambda m : chr(96+int(m[0])), x)
+
+def handle_roman(x) -> str:
+    _to_roman_ind = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+    _to_roman = {
+        1     : 'I'  ,
+        4     : 'IV' ,
+        5     : 'V'  ,
+        9     : 'IX' ,
+        10    : 'X'  ,
+        40    : 'XL' ,
+        50    : 'L'  ,
+        90    : 'XC' ,
+        100   : 'C'  ,
+        400   : 'CD' ,
+        500   : 'D'  ,
+        900   : 'CM' ,
+        1000  : 'M'  
+    }
+    
+    pos = 12
+    num = int(x)
+    res = ""
+    while num:
+        while(num < _to_roman_ind[pos]):
+            pos -=1
+        res += _to_roman[pos]
+        num -= _to_roman_ind[pos]
+    return res
+
 def roman(x):
-    return x
-def left(x):
-    return x
-def center(x,):
-    return x
-def right(x):
-    return x
-def allbutlast(x):
-    return x
+    return sub(r"\d+", handle_roman(x), x)        
+
+def left(x, n, l, r):
+    res = ""
+    tam = 0
+    if l:
+        res += l
+        tam += len(l)
+    res += x
+    tam += len(x)
+    if r:
+        res += (n-tam-len(r)) * " "
+        res += r
+    else: 
+        res += (n-tam) * " "
+    return res
+    
+def center(x, n, l, r):
+    res = ""
+    tam = 0
+    if l:
+        res += l
+        tam += len(l)
+    if r:
+        numSpaces = (n - tam - len(x) - len(r)) / 2
+        res += ceil(numSpaces) * " "
+        res += x
+        res += floor(numSpaces) * " "
+        res += r
+    else:
+        numSpaces = (n - tam - len(x)) / 2
+        res += ceil(numSpaces) * " "
+        res += x
+        res += floor(numSpaces) * " "
+    return res
+
+def right(x, n, l, r):
+    res = ""
+    tam = 0
+    if l:
+        res += l
+        tam += len(l)
+    if r:
+        res += (n-tam-len(r)) * " "
+        res += x
+        res += r
+    else:
+        res += (n-tam) * " "
+        res += x
+    return res
+
 
 class Pipe:
     _pipes = {
@@ -44,9 +129,9 @@ class Pipe:
         'nowrap'    : lambda x : nowrap(x),
         'alpha'     : lambda x : alpha(x),
         'roman'     : lambda x : roman(x),
-        'left'      : lambda n, x, value : left(n, x, value),
-        'center'    : lambda n, x, value : center(n, x, value),
-        'right'     : lambda n, x, y, value : right(n, x, y, value)
+        'left'      : lambda x, n, l, r : left(x, n, l, r),
+        'center'    : lambda x, n, l, r : center(x, n, l, r),
+        'right'     : lambda x, n, l, r : right(x, n, l, r)
     }
     def __init__(self, id, args=None) -> None:
         self.id = id
