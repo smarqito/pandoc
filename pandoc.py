cd46be864ad7f3ddb81e8ec8253eb74dc9d5e316
Marco António Sousa <smarqito@gmail.com>
2022-05-14 17:28:31 +0100

    add idea

--- pandoc.py
+++ pandoc.py
@@ -1,45 +1,57 @@
-#!/usr/bin/env python3 
-#----------------------------------------------------------------
+#!/usr/bin/env python3
+# ----------------------------------------------------------------
 # Created by: G53
 # Created date: 2022-05-08
 # Version = '1.0'
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 ''' pandoc.py: Ficheiro de inicialização our_pandoc '''
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 from re import *
 import sys
 import yaml
 import getopt
+import os
 from pandoc_par import parser, getFinfo
+
 
 # f = open("samples/data.yaml", "r")
 
 # loaded = yaml.load(f.read(), Loader=yaml.Loader)
 
-args_filter = 'd:o:i:hg'
+args_filter = 'd:o:hge:'
 long_args = ['help=']
 
-t_info = {}
+t_info = {
+    'files': []
+}
+
 
 def throw_error(msg, to_exit=False):
     print(msg, file=sys.stderr)
     if to_exit:
         exit()
 
+
 def handle_input(x):
     global t_info
     info = getFinfo(x)
-    
+
     try:
         sys.stdin = open(x, 'r')
     except:
         throw_error(f"template de input {x} nao encontrado!", True)
-    
+
     t_info['finfo'] = info
+
+
+def set_input(x: list):
+    global t_info
+    t_info['files'] = x
+
 
 def handle_data(x):
     global t_info
-    
+
     try:
         d = open(x, 'r')
     except:
@@ -47,12 +59,15 @@ def handle_data(x):
 
     t_info['yaml'] = yaml.load(d.read(), Loader=yaml.Loader)
 
+
 def handle_output(x):
     f = open(x, 'w')
     sys.stdout = f
 
+
 def handle_help(arg):
     print("help type:", arg)
+
 
 def handle_log(file):
     f = open(file, 'a')
@@ -60,12 +75,14 @@ def handle_log(file):
 
 
 args_handler = {
-    '-i' : handle_input,
-    '-d' : handle_data,
-    '-o' : handle_output,
+    'input': handle_input,
+    '-d': handle_data,
+    '-o': handle_output,
     '--help': handle_help,
-    '--log' : handle_log
+    '--log': handle_log,
+    '-e': handle_log
 }
+
 
 def handle_args():
     global t_info
@@ -78,21 +95,31 @@ def handle_args():
             args_handler['--help'](h)
         exit()
     '''se chegou aqui, nao tem helpers (exit...)'''
+    if not args:
+        throw_error(f'Arguments not defined!!', True)
     # ordena por chave do tuplo (nao faz redirect do output ate ter info toda!)
     # optlist.sort(key=lambda y : y[0])
     for opt, value in optlist:
         args_handler[opt](value)
+    set_input(args)
+
+# define pasta output
+# define extensao ficheiros
+# processa os argumentos, redireciona o output para o PATH+FILENAME+EXTENSAO
 
 def hdoc():
     global t_info
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 4:
         print('mal construido')
         throw_error("hdoc -d <yaml_file> <template_file> ", True)
     handle_args()
-    parser.finfo = t_info['finfo']
-    parser.yaml = t_info['yaml']
-    txt = sys.stdin.read()
-    result = parser.parse(txt)
-    result.pp()
+    for filename in t_info['files']:
+        handle_input(filename)
+        parser.finfo = t_info['finfo']
+        parser.yaml = t_info['yaml']
+        txt = sys.stdin.read()
+        result = parser.parse(txt)
+        result.pp()
+
 
 hdoc()
