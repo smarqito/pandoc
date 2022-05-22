037754e5180f9d45205328881d6931edcf1eb958
Marco António Sousa <smarqito@gmail.com>
2022-05-22 18:07:50 +0100

    script examples

--- ex.sh
+++ ex.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
 echo '#############################'
-echo '        Exemplo default'
+echo '            BEM VINDO        '
+echo '  SCRIPT DE TESTES GRUPO 57  '
 echo '#############################'
-pandoc.py -i samples/info.yaml samples/ex1.in
+echo ''
+clean=false
 
-rm -r __pycache__ parser.out parsetab.py
+
+function clean() {
+    rm -r __pycache__ parser.out parsetab.py
+}
+case $1 in
+    "default")
+    echo '#############################'
+    echo '        Exemplo default      '
+    echo 'pandoc.py -i samples/info.yaml samples/ex1.in'
+    echo '#############################'
+    echo ''
+    pandoc.py -i samples/info.yaml samples/ex1.in
+    clean
+    ;;
+    "help")
+    echo '#############################'
+    echo '        Exemplo help         '
+    echo 'pandoc.py -h                 '
+    echo '#############################'
+    echo ''
+    pandoc.py -h
+    clean
+    ;;
+    "input")
+    echo '##############################'
+    echo '        Exemplo redirect input'
+    echo 'pandoc.py -i samples/info.yaml -f samples/ ex1.in'
+    echo '##############################'
+    echo ''
+    pandoc.py -i samples/info.yaml -f samples/ ex1.in
+    clean
+    ;;
+    "output")
+    echo '##############################'
+    echo '        Exemplo redirect output'
+    echo 'pandoc.py -i samples/info.yaml -t' $2/$3 'ex1.in'
+    echo '##############################'
+    echo ''
+    pandoc.py -i samples/info.yaml -t $2/$3 samples/ex1.in
+    clean
+    ;;
+    "comments")
+    echo '##############################'
+    echo '        Exemplo redirect output & print comments'
+    echo 'pandoc.py -i samples/info.yaml -t' $2/$3 'ex1.in'
+    echo '##############################'
+    echo ''
+    pandoc.py -i samples/info.yaml -t $2/$3 --oc=$4 --cc=$5 samples/ex1.in
+    clean
+    ;;
+esac
